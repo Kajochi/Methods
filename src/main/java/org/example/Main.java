7f7cc11d1a0dca1c1f51cb3e3926de6348104d03
christophgroneberg <christophgroneberg1@gmx.de>
2023-05-31 14:47:43 +0200

    add new methods

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = sumMethod(3,5);
+        //System.out.println(returnString(6,2));
+        //System.out.println(faculty(5));
+        System.out.println(facultyWhile(5));
+    }
+
+    public static long facultyWhile(int num){
+        long result = 1;
+        long counter = 1;
+
+        while(counter < num){
+            result *= counter;
+            counter++;
+        }
+
+        return result;
+    }
+    public static int faculty(int num){
+        int result = 1;
+        for (int i = 1; i <= num; i++){
+            result *= i;
+        }
+        return result;
+
+    }
+
+    public static int facultyDecrement(int n){
+
+        for(int i = n; i>1; i--){
+            n *= (i-1);
+        }
+        return n;
+    }
+    public static int sumMethod(int num1, int num2){
+        return num1 +  num2;
+    }
+
+    public static String returnString(int num1, int num2){
+        return "Summe: " + (num1 + num2);
+
+    }
+}
